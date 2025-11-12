8e76de6dcc4af1bab9d0a6ee61e2b86419e4c482
Yassine <Yassine.bouabdallah@esprit.tn>
2025-11-12 02:21:52 +0100

    Adding task and rewawrd MS

--- api-gateway/src/main/java/com/wala/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/wala/api_gateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.wala.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
